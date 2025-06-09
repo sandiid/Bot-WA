df018ad1b35a2cae214b40ea5b5022eef21d546c
Sandi <66204938+sandiid@users.noreply.github.com>
2025-06-09 17:45:07 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  }
+});
+
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('✅ Bot siap!');
+});
+
+client.on('message', msg => {
+  if (msg.body === '!ping') {
+    msg.reply('pong 🏓');
+  }
+});
+
+client.initialize();
